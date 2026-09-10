f393f90c57c73c16234f25fb54339bcdcde50144
Cristian <cri_1112@hotmail.com>
2026-09-09 23:11:36 -0300

    cambio de contacto

--- src/data/contacto.js
+++ src/data/contacto.js
@@ -2,7 +2,7 @@
 // El número de WhatsApp va con código de país y sin "+", espacios ni guiones.
 // Ejemplo Uruguay: 598 + 9 dígitos del celular sin el 0 inicial.
 
-export const WHATSAPP_NUMERO = '59899000000'; // TODO: poner el número real
+export const WHATSAPP_NUMERO = '59895532454'; // TODO: poner el número real
 export const INSTAGRAM_USUARIO = 'soyolis.uy';
 export const UBICACION = 'Young, Río Negro · Montevideo';
 
